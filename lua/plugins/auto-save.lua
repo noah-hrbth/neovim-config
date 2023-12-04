a0942ca5ec4d61cda93569d8dd9a8ec0bc015adb
Noah <noah.harborth.private@gmail.com>
2023-12-04 21:21:43 +0100

    init my own neovim config

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,10 @@
+return {
+  "pocco81/auto-save.nvim",
+  opts = {
+    execution_message = {
+      message = "",
+      dim = 0.0,
+      cleaning_interval = 0,
+    },
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "oxocarbon",
+      },
+    },
+  },
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "nyoom-engineering/oxocarbon.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+  },
+}
